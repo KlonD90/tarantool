a337efd821f290b415038c01e1689ffa895db26f
Veniamin Gvozdikov <vgvozdikov@tarantool.org>
2015-07-10 14:01:57 +0300

    Added support setup group for run tarantool with custom group by tarantoolctl

--- src/box/lua/load_cfg.lua
+++ src/box/lua/load_cfg.lua
@@ -54,7 +54,8 @@ local default_cfg = {
     custom_proc_title   = nil,
     pid_file            = nil,
     background          = false,
-    username            = nil ,
+    username            = nil,
+    groupname           = nil,
     coredump            = false,
 
     -- snapshot_daemon
@@ -101,6 +102,7 @@ local template_cfg = {
     pid_file            = 'string',
     background          = 'boolean',
     username            = 'string',
+    groupname           = 'string',
     coredump            = 'boolean',
     snapshot_period     = 'number',
     snapshot_count      = 'number',
